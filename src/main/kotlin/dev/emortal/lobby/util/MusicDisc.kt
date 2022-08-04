e2fc34d4014e7fc52215323f9475cbbfe2aa0d17
emortal <me@emortal.dev>
2022-08-04 06:05:29 +0100

    Re-order based on wiki

--- src/main/kotlin/dev/emortal/lobby/util/MusicDisc.kt
+++ src/main/kotlin/dev/emortal/lobby/util/MusicDisc.kt
@@ -11,8 +11,6 @@ enum class MusicDisc(
     val length: Int
 ) {
     MUSIC_DISC_13(Material.MUSIC_DISC_13, SoundEvent.MUSIC_DISC_13, "13", "C418 - 13", 178),
-    MUSIC_DISC_11(Material.MUSIC_DISC_11, SoundEvent.MUSIC_DISC_11, "11", "C418 - 11", 71),
-    MUSIC_DISC_5(Material.MUSIC_DISC_5, SoundEvent.MUSIC_DISC_5, "5", "Samuel Åberg - 5", 178),
     MUSIC_DISC_CAT(Material.MUSIC_DISC_CAT, SoundEvent.MUSIC_DISC_CAT, "cat", "C418 - cat", 185),
     MUSIC_DISC_BLOCKS(Material.MUSIC_DISC_BLOCKS, SoundEvent.MUSIC_DISC_BLOCKS, "blocks", "C418 - blocks", 345),
     MUSIC_DISC_CHIRP(Material.MUSIC_DISC_CHIRP, SoundEvent.MUSIC_DISC_CHIRP, "chirp", "C418 - chirp", 185),
@@ -22,6 +20,7 @@ enum class MusicDisc(
     MUSIC_DISC_STAL(Material.MUSIC_DISC_STAL, SoundEvent.MUSIC_DISC_STAL, "stal", "C418 - stal", 150),
     MUSIC_DISC_STRAD(Material.MUSIC_DISC_STRAD, SoundEvent.MUSIC_DISC_STRAD, "strad", "C418 - strad", 188),
     MUSIC_DISC_WARD(Material.MUSIC_DISC_WARD, SoundEvent.MUSIC_DISC_WARD, "ward", "C418 - ward", 251),
+    MUSIC_DISC_11(Material.MUSIC_DISC_11, SoundEvent.MUSIC_DISC_11, "11", "C418 - 11", 71),
     MUSIC_DISC_WAIT(Material.MUSIC_DISC_WAIT, SoundEvent.MUSIC_DISC_WAIT, "wait", "C418 - wait", 228),
     MUSIC_DISC_PIGSTEP(
         Material.MUSIC_DISC_PIGSTEP,
@@ -36,7 +35,8 @@ enum class MusicDisc(
         "otherside",
         "Lena Raine - otherside",
         195
-    );
+    ),
+    MUSIC_DISC_5(Material.MUSIC_DISC_5, SoundEvent.MUSIC_DISC_5, "5", "Samuel Åberg - 5", 178);
 
 
     companion object {
